f7dc0b062ddb15ccde363924ae7b1df77455c909
mdmahidmahmud <69872745+mdmahidmahmud@users.noreply.github.com>
2020-08-21 16:51:10 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.player-play').magnificPopup({
+        items: ({
+            src: 'https://www.youtube.com/watch?v=eEzD-Y97ges',
+            type: 'iframe'
+        })
+
+    });
+});
